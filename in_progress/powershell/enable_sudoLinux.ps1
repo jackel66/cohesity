9e652825b9ccbea6e70c70b940bf1102d0cbdd9c
Douglas Austin <douglas_austin08@hotmail.com>
2025-05-20 16:02:05 -0700

    created sudo script

--- in_progress/powershell/enable_sudoLinux.ps1
+++ in_progress/powershell/enable_sudoLinux.ps1
@@ -1,3 +1,29 @@
+<#
+.SYNOPSIS
+Exports Cohesity user and group accounts (non-LOCAL) from a cluster.
+
+.DESCRIPTION
+Connects to a Cohesity cluster and exports all AD/SSO users and groups (excluding LOCAL) into a JSON file.
+
+.PARAMETER vip
+The FQDN or IP address of the Cohesity cluster.
+
+.PARAMETER user
+The username for Cohesity authentication.
+
+.PARAMETER domain
+The domain of the user account (e.g., LOCAL or AD domain).
+
+.EXAMPLE
+.\export_roles.ps1 -vip cohesity.domain.com -user user01 -domain domain.com
+
+.NOTES
+Author: Doug Austin  
+Date: 05/12/2025  
+Changelog:
+  - Initial version for exporting accounts
+  - Refactored for dynamic input and help
+#>
 # Load Cohesity API Helper
 . "$PSScriptRoot\cohesity-api.ps1"
 
